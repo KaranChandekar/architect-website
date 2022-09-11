18db75540e700ad6199ea7e35e413494fee4d820
Karan <karanchandekar431@gmail.com>
2022-09-11 12:31:06 +0530

    made loading spinner work

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 4000);
+};
+
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
