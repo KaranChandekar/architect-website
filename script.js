82672f36d0478c979dc0acaa46271f48427dca73
Karan <karanchandekar431@gmail.com>
2022-09-11 12:16:47 +0530

    made scroll button work

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
+
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 1000);
+});
